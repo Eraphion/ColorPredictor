03b126050cdcebf547e88b2bc190e4d20e99622f
LENOVOWORK\Evgeny <official.trusov.mail@gmail.com>
2020-01-22 07:00:19 +0700

    added algorithm

--- server/api.py
+++ server/api.py
@@ -12,7 +12,7 @@ def normalize_params(params):
     normalized_r = params['R']/255
     normalized_g = params['G']/255
     normalized_b = params['B']/255
-    normalized_background = 0
+    normalized_background = 1
     if params.get('color') == 'white':
         normalized_background = 0
     return {
@@ -27,15 +27,17 @@ def add_user_choice():
     data = request.data.decode('utf-8')
     params = ast.literal_eval(data)
     with open('userChoices.txt', 'a') as f:
+        f.seek(0,2)
+        f.write('|')
         json.dump(normalize_params(params),f)
     return jsonify(success=True)
 
 @app.route("/api/pickColor", methods=['GET'])
 def pick_color():
     normalized_params = {
-        'r': int(request.args.get('R')) / 255,
-        'g': int(request.args.get('G')) / 255,
-        'b': int(request.args.get('B')) / 255,
+        'r': float(request.args.get('R')) / 255,
+        'g': float(request.args.get('G')) / 255,
+        'b': float(request.args.get('B')) / 255,
     }
     color = predict_color(normalized_params)
     return jsonify(color)
--- server/predictor.py
+++ server/predictor.py
@@ -1,4 +1,30 @@
+import ast
+
 def predict_color(normalized_params):
+    MAX_DISTANCE = 30
+    src_r = normalized_params['r']
+    src_g = normalized_params['g']
+    src_b = normalized_params['b']
+    koeff_sum = 0
+    with open('userChoices.txt') as choices_file:
+        choices = choices_file.read().split('|')
+        for choice in choices:
+            data_item = ast.literal_eval(choice)
+            r = float(data_item['r'])
+            g = float(data_item['g'])
+            b = float(data_item['b'])
+            background = int(data_item['background'])
+            if background == 1:
+                koeff = 1
+            else:
+                koeff = -1
+            dist = ((r-src_r)**2 + (g-src_g)**2 + (b-src_b)**2)**0.5
+            if dist < MAX_DISTANCE:
+                koeff_sum = koeff_sum + koeff/(dist**1.8)
+    if koeff_sum > 0:
+        background = 'black'
+    else:
+        background = 'white'
     return {
-        'backgroundColor': 'black'
+        'backgroundColor': background
     }
